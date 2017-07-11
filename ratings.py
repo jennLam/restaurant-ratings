199280577847f16f8d43e71210ff0dc72754869d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-10 18:02:18 -0700

    Update with random

--- ratings.py
+++ ratings.py
@@ -1,3 +1,5 @@
+import random
+
 """Restaurant rating lister."""
 
 restaurant_ratings = {}
@@ -29,15 +31,11 @@ def sort_dict(dict_name):
 
     #loops through list, prints restaurant name and its rating
     for rest in sorted_rest:
-        print rest, "is rated at", dict_name[rest] + "."
+        print rest, "is rated at", str(dict_name[rest]) + "."
 
-
-def get_rest_input(dict_name):
-    """Takes in user input and adds to dictionary"""
-
-    rest_input = raw_input("Enter a restaurant name: ").title()
-
-    #validate user input, checks if rating is an integer and between 1 and 5
+def validate_rating():
+    """Checks user input is an integer and between 1 and 5"""
+    
     while True:
         try:
             rating_input = raw_input("Enter a rating: ")
@@ -49,14 +47,42 @@ def get_rest_input(dict_name):
         except:
             print "Please enter a number between 1 and 5: "
 
+    return rating_int
+
+def get_rest_input(dict_name):
+    """Takes in user input and adds to dictionary"""
+
+    rest_input = raw_input("Enter a restaurant name: ").title()
+
+    rating_input = validate_rating()
     #adds user inputs to dictionary
     dict_name[rest_input] = rating_input
     return dict_name
 
 
+def rate_random_rest(dict_name):
+    """Chooses random restaurant and lets user re-rate it"""
+
+    sorted_rest = sorted(dict_name)
+
+    random_rest = random.choice(sorted_rest)
+
+    print "Random restaurant is:", random_rest
+    print "Its rating is:", dict_name[random_rest]
+
+    rating_input = validate_rating()
+    print rating_input
+
+    dict_name[random_rest] = rating_input
+
+    return dict_name
+
+
+
 def print_menu():
     """Prints menu and gets user input"""
 
+    read_file("scores.txt")
     print "Welcome to the restaurant rater!"
     print
 
@@ -66,6 +92,7 @@ def print_menu():
         print "************************"
         print "1. See all restaurants"
         print "2. Add a new restaurant"
+        print "3. Rate random restaurant"
         print "Type 'q' to quit"
         print "************************"
 
@@ -78,10 +105,11 @@ def get_menu_input():
     choice = raw_input("> ")
 
     if choice == '1':
-        read_file("scores.txt")
         sort_dict(restaurant_ratings)
     elif choice == '2':
         get_rest_input(restaurant_ratings)
+    elif choice == '3':
+        rate_random_rest(restaurant_ratings)
     elif choice == 'q':
         exit()
     else:
