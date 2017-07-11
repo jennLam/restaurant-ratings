81036f631d9fa5bc617a69929cb6072d88b90eb5
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-10 17:25:20 -0700

    Updated restaurant ratings with comments

--- ratings.py
+++ ratings.py
@@ -1,21 +1,43 @@
 """Restaurant rating lister."""
 
-rest_output = {}
+restaurant_ratings = {}
+
+
+def read_file(filename):
+    """Reads a file and stores info in a dictionary"""
+
+    restaurant_file = open(filename)
+
+    #loop through each line in file
+    for line in restaurant_file:
+        #strip whitespace at end of line
+        line = line.rstrip()
+        #split line and store in list using ":"" as delimiter
+        rest_and_ratings = line.split(":")
+        rest_name = rest_and_ratings[0]
+        rating_num = rest_and_ratings[1]
+        #add restaurant name and rating to dictionary
+        restaurant_ratings[rest_name] = rating_num
+
+    restaurant_file.close()
 
 def sort_dict(dict_name):
     """Sorts and outputs a list of restaurants and their ratings"""
 
+    #alphabetically sorts restaurant names and stores in list
     sorted_rest = sorted(dict_name)
 
+    #loops through list, prints restaurant name and its rating
     for rest in sorted_rest:
         print rest, "is rated at", dict_name[rest] + "."
 
 
-def get_input(dict_name):
+def get_rest_input(dict_name):
     """Takes in user input and adds to dictionary"""
 
     rest_input = raw_input("Enter a restaurant name: ").title()
 
+    #validate user input, checks if rating is an integer and between 1 and 5
     while True:
         try:
             rating_input = raw_input("Enter a rating: ")
@@ -27,56 +49,43 @@ def get_input(dict_name):
         except:
             print "Please enter a number between 1 and 5: "
 
-
+    #adds user inputs to dictionary
     dict_name[rest_input] = rating_input
     return dict_name
 
 
-def restaurant_ratings(filename):
-    """Reads and sorts file"""
+def print_menu():
+    """Prints menu and gets user input"""
 
-    read_file(filename)
-
-    sort_dict(rest_output)
-
-    restaurant_file.close()
-
-    rest_output = get_input(rest_output)
-
-    sort_dict(rest_output)
-
-def read_file(filename):
-
-    restaurant_file = open(filename)
-
-    for line in restaurant_file:
-        line = line.rstrip()
-        rest_and_ratings = line.split(":")
-        rest_output[rest_and_ratings[0]] = rest_and_ratings[1]
-
-
-print "Welcome to the restaurant rater!"
-print
-
-while True:
-
+    print "Welcome to the restaurant rater!"
     print
-    print "************************"
-    print "1. See all restaurants"
-    print "2. Add a new restaurant"
-    print "Type 'q' to quit"
-    print "************************"
+
+    while True:
+
+        print
+        print "************************"
+        print "1. See all restaurants"
+        print "2. Add a new restaurant"
+        print "Type 'q' to quit"
+        print "************************"
+
+        #gets user inputs for the menu
+        get_menu_input()
+
+def get_menu_input():
+    """Takes in user input and calls appropriate functions"""
 
     choice = raw_input("> ")
 
     if choice == '1':
         read_file("scores.txt")
-        sort_dict(rest_output)
+        sort_dict(restaurant_ratings)
     elif choice == '2':
-        get_input(rest_output)
+        get_rest_input(restaurant_ratings)
     elif choice == 'q':
         exit()
     else:
         print "Enter 1, 2, or q"
-        print 
-#restaurant_ratings("scores.txt")
+        print
+
+print_menu()
